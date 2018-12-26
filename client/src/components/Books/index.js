ad5d4dba25049f75dfa84d86539d53e0586b6785
Shoaib <shoaibkhalid1996@gmail.com>
2018-12-26 12:13:46 +0500

    Changed DB config files

--- client/src/components/Books/index.js
+++ client/src/components/Books/index.js
@@ -1,70 +1,57 @@
-import React, { Component } from 'react';
-import { getBookWithReviewer, clearBookWithReviewer } from '../../actions';
-import { connect } from 'react-redux';
+import React, { Component } from "react";
+import { getBookWithReviewer, clearBookWithReviewer } from "../../actions";
+import { connect } from "react-redux";
 
 class BookView extends Component {
+  componentWillMount() {
+    this.props.dispatch(getBookWithReviewer(this.props.match.params.id));
+  }
 
-    componentWillMount() {
-        this.props.dispatch(getBookWithReviewer(this.props.match.params.id))
-    }
+  componentWillUnmount() {
+    this.props.dispatch(clearBookWithReviewer());
+  }
 
-    componentWillUnmount() {
-        this.props.dispatch(clearBookWithReviewer())
-    }
+  renderBook = books =>
+    books.book ? (
+      <div className="br_container">
+        <div className="br_header">
+          <h2>{books.book.name}</h2>
+          <h5>{books.book.author}</h5>
+          <div className="br_reviewer">
+            <span>Review by:</span> {books.reviewer.firstname}{" "}
+            {books.reviewer.lastname}
+          </div>
+        </div>
+        <div className="br-review">{books.book.review}</div>
 
-  
-
-
-    renderBook = (books) => (
-        books.book ?
-            <div className='br_container'>
-                <div className='br_header'>
-                    <h2>{books.book.name}</h2>
-                    <h5>{books.book.author}</h5>
-                    <div className='br_reviewer'>
-                        <span>Review by:</span> {books.reviewer.firstname} {books.reviewer.lastname}
-                    </div>
-                </div>
-                <div className="br-review">
-                    {books.book.review}
-                </div>
-
-                <div className='br_box'>
-                    <div className='left'>
-                        <div>
-                            <span>Pages:</span> {books.book.pages}
-                        </div>
-                        <div>
-                            <span>Price:</span>  {books.book.price}
-                        </div>
-
-                    </div>
-
-                    <div className='right'>
-                        <span>Rating</span>
-                        <div>{books.book.rating}/5</div>
-                    </div>
-
-                </div>
-            </div>
-            : null
-    )
-
-    render() {
-        console.log(this.props);
-        let books = this.props.books;
-        return (
+        <div className="br_box">
+          <div className="left">
             <div>
-                {this.renderBook(books)}
+              <span>Pages:</span> {books.book.pages}
             </div>
-        );
-    }
+            <div>
+              <span>Price:</span> {books.book.price}
+            </div>
+          </div>
+
+          <div className="right">
+            <span>Rating</span>
+            <div>{books.book.rating}/5</div>
+          </div>
+        </div>
+      </div>
+    ) : null;
+
+  render() {
+    let books = this.props.books;
+    return <div>{this.renderBook(books)}</div>;
+  }
 }
 
-const mapStateToProps = (state) => {
-    return {
-        books: state.books
-    }
-}
+const mapStateToProps = state => {
+  return {
+    books: state.books
+  };
+};
 
-export default connect(mapStateToProps)(BookView)
+export default connect(mapStateToProps)(BookView);
