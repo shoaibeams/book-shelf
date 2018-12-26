889579ae000490f0acf7d17301dd4f71549913c1
Shoaib <shoaibkhalid1996@gmail.com>
2018-12-26 12:26:28 +0500

    Fixed process.env.PORT not port

--- server/server.js
+++ server/server.js
@@ -30,28 +30,28 @@ app.use;
 //GET//
 
 app.get("/api/auth", auth, (req, res) => {
-	res.json({
-		isAuth: true,
-		id: req.user._id,
-		email: req.user.email,
-		firstname: req.user.firstname,
-		lastname: req.user.lastname
-	});
+  res.json({
+    isAuth: true,
+    id: req.user._id,
+    email: req.user.email,
+    firstname: req.user.firstname,
+    lastname: req.user.lastname
+  });
 });
 
 app.get("/api/logout", auth, (req, res) => {
-	req.user.deleteToken(req.token, (err, user) => {
-		if (err) return res.status(400).send(err);
-		res.sendStatus(200);
-	});
+  req.user.deleteToken(req.token, (err, user) => {
+    if (err) return res.status(400).send(err);
+    res.sendStatus(200);
+  });
 });
 
 app.get("/api/getBook", (req, res) => {
-	let id = req.query.id;
-	Book.findById(id, (err, doc) => {
-		if (err) return res.status(400).send(err);
-		res.send(doc);
-	});
+  let id = req.query.id;
+  Book.findById(id, (err, doc) => {
+    if (err) return res.status(400).send(err);
+    res.send(doc);
+  });
 });
 
 app.get("/api/books", (req, res) => {
@@ -62,121 +62,121 @@ app.get("/api/books", (req, res) => {
 
   //order = asc || desc
   Book.find()
-  .skip(skip)
-  .sort({ _id: order })
-  .limit(limit)
-  .exec((err, doc) => {
-  	if (err) return res.status(400).send(err);
-  	res.send(doc);
-  });
+    .skip(skip)
+    .sort({ _id: order })
+    .limit(limit)
+    .exec((err, doc) => {
+      if (err) return res.status(400).send(err);
+      res.send(doc);
+    });
 });
 
 app.get("/api/getReviewer", (req, res) => {
-	let id = req.query.id;
-	User.findById(id, (err, doc) => {
-		if (err) return res.status(400).send(err);
-		res.json({
-			firstname: doc.firstname,
-			lastname: doc.lastname
-		});
-	});
+  let id = req.query.id;
+  User.findById(id, (err, doc) => {
+    if (err) return res.status(400).send(err);
+    res.json({
+      firstname: doc.firstname,
+      lastname: doc.lastname
+    });
+  });
 });
 
 app.get("/api/users", (req, res) => {
-	User.find({}, (err, users) => {
-		if (err) return res.status(400).send(err);
-		res.status(200).json(users);
-	});
+  User.find({}, (err, users) => {
+    if (err) return res.status(400).send(err);
+    res.status(200).json(users);
+  });
 });
 
 app.get("/api/user_posts", (req, res) => {
-	Book.find({ ownerId: req.query.user }).exec((err, docs) => {
-		if (err) return res.status(400).send(err);
-		res.status(200).json(docs);
-	});
+  Book.find({ ownerId: req.query.user }).exec((err, docs) => {
+    if (err) return res.status(400).send(err);
+    res.status(200).json(docs);
+  });
 });
 
 //POST//
 app.post("/api/book", (req, res) => {
-	const book = new Book(req.body);
-	book.save((err, doc) => {
-		if (err) return res.status(400).send(err);
-		res.status(200).json({
-			post: true,
-			bookId: doc._id
-		});
-	});
+  const book = new Book(req.body);
+  book.save((err, doc) => {
+    if (err) return res.status(400).send(err);
+    res.status(200).json({
+      post: true,
+      bookId: doc._id
+    });
+  });
 });
 
 app.post("/api/register", (req, res) => {
-	const user = new User(req.body);
-	user.save((err, doc) => {
-		if (err) return res.json({ success: false });
-		res.status(200).json({
-			success: true,
-			user: doc
-		});
-	});
+  const user = new User(req.body);
+  user.save((err, doc) => {
+    if (err) return res.json({ success: false });
+    res.status(200).json({
+      success: true,
+      user: doc
+    });
+  });
 });
 
 app.post("/api/login", (req, res) => {
-	User.findOne({ email: req.body.email }, (err, user) => {
-		if (!user)
-			return res.json({
-				isAuth: false,
-				message: "Auth Failed, email not found"
-			});
+  User.findOne({ email: req.body.email }, (err, user) => {
+    if (!user)
+      return res.json({
+        isAuth: false,
+        message: "Auth Failed, email not found"
+      });
 
-		user.comparePassword(req.body.password, (err, isMatch) => {
-			if (!isMatch)
-				return res.json({
-					isAuth: false,
-					message: "Wrong password"
-				});
+    user.comparePassword(req.body.password, (err, isMatch) => {
+      if (!isMatch)
+        return res.json({
+          isAuth: false,
+          message: "Wrong password"
+        });
 
-			user.generateToken((err, user) => {
-				if (err) return res.status(400).send(err);
-				res.cookie("auth", user.token).json({
-					isAuth: true,
-					id: user._id,
-					email: user.email
-				});
-			});
-		});
-	});
+      user.generateToken((err, user) => {
+        if (err) return res.status(400).send(err);
+        res.cookie("auth", user.token).json({
+          isAuth: true,
+          id: user._id,
+          email: user.email
+        });
+      });
+    });
+  });
 });
 
 //DELETE//
 
 app.delete("/api/delete_book", (req, res) => {
-	let id = req.query.id;
-	Book.findByIdAndRemove(id, (err, doc) => {
-		if (err) return res.status(400).send(err);
-		res.json(true);
-	});
+  let id = req.query.id;
+  Book.findByIdAndRemove(id, (err, doc) => {
+    if (err) return res.status(400).send(err);
+    res.json(true);
+  });
 });
 
 //UPDATE//
 
 app.post("/api/book_update", (req, res) => {
-	Book.findByIdAndUpdate(req.body._id, req.body, { new: true }, (err, doc) => {
-		if (err) return res.status(400).send(err);
-		res.json({
-			success: true,
-			doc
-		});
-	});
+  Book.findByIdAndUpdate(req.body._id, req.body, { new: true }, (err, doc) => {
+    if (err) return res.status(400).send(err);
+    res.json({
+      success: true,
+      doc
+    });
+  });
 });
 
 //Serve static routes if in production
 if (process.env.NODE_ENV === "production") {
-	const path = require("path");
-	app.get("/*", (req, res) => {
-		res.sendfile(path.resolve(__dirname, "../client/", "build", "index.html"));
-	});
+  const path = require("path");
+  app.get("/*", (req, res) => {
+    res.sendfile(path.resolve(__dirname, "../client/", "build", "index.html"));
+  });
 }
 
-const port = process.env.port || 3001;
+const port = process.env.PORT || 3001;
 app.listen(port, () => {
-	console.log("Server is listening at " + port);
+  console.log("Server is listening at " + port);
 });
